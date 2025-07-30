ffa0caace9759bc133dd735a9255fe6255fa2208
Noah Brelage <noah.brelage@outlook.de>
2025-07-30 23:48:03 +0200

    fetch API data

--- fetch_tagesschau.py
+++ fetch_tagesschau.py
@@ -1,34 +1,41 @@
 import requests
-import yaml
-from utils import setup_logger
+import os
+import logging
+from datetime import datetime
+from utils import setup_logger, load_source
 
-with open("sources.yaml", "r") as file:
-    data = yaml.safe_load(file)
-    tagesschau = data["tagesschau"]
-
-logger = setup_logger()
 
 def main():
-    scrape_tagesschau_headlines()
+    fetcher = Fetcher()
+    fetcher.fetch_tagesschau_headlines()
 
-def scrape_tagesschau_headlines():
-    data = requests.get(tagesschau).json()
-    logger.info("reached Tagesschau API")
-    
-    news = data["news"]
-    for item in news[:5]:
-        headline = item["title"]
-        link = item["detailsweb"]
-        content = item.get("content", [])
-        text = ""
-        for entry in content[:2]:
-            text += "  -" + entry.get("value", "") + "\n"
+class Fetcher:
+    def __init__(self):
+        self.logger = setup_logger(__name__)
+        self.source = load_source("tagesschau")
+        self.today = datetime.now().date()
+
+
+    def fetch_tagesschau_headlines(self):
+        data = requests.get(self.source).json()
+        self.logger.info("reached Tagesschau API")
         
-        aricle = f"""
-        {headline}
-        {link}
-        {text}
-        """
+        tagesschau_news = []
+        news = data["news"]
+        for item in news[:5]:
+            headline = item["title"]
+            link = item["detailsweb"]
+            content = item.get("content", [])
+            text = ""
+            for entry in content[:2]:
+                text += entry.get("value", "")
+            tagesschau_news.append([headline, link, text])
+
+        if self.logger.isEnabledFor(logging.DEBUG):
+            os.makedirs("workbench", exist_ok=True)
+            with open(f"workbench/tagesschau{self.today}.html", "w") as file:
+                    file.write(str(tagesschau_news))
+
 
 
 if __name__ == "__main__":
