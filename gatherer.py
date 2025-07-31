55702c77470f115feebed4414e4de554e205bbba
Noah Brelage <noah.brelage@outlook.de>
2025-07-31 12:02:22 +0200

    add scalable parallel processing

--- gatherer.py
+++ gatherer.py
@@ -1,8 +1,12 @@
 import subprocess
 
 def main():
-    subprocess.run(["python3", "scrape_zeit.py"])
-    subprocess.run(["python3", "fetch_tagesschau.py"])
+    procs = [
+        subprocess.Popen(["python3", "scrape_zeit.py"]),
+        subprocess.Popen(["python3", "fetch_tagesschau.py"])
+    ]
+    for p in procs:
+        p.wait()
 
 
 if __name__ == "__main__":
